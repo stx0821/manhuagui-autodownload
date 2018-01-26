81ebc1822c62b6f7eb35743801874d089b009814
stx0821 <314269395@qq.com>
2018-01-26 11:38:30 +0800

    1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	path:'/comic/4731/',	//下载漫画的URL，如此URL下载的地址是http://www.manhuagui.com/comic/26886/
+	path:'/comic/4731/',	//下载漫画的URL，比如URL下载的地址是http://www.manhuagui.com/comic/26886/
 	downloadDir:'download',	//下载文件夹
 	timeout:1000	//下载间隔，值太小请求太多容易超时，太大则下载会慢，根据自己情况调整
 }
